6d82bce61ae39c343673299460ee91713bbed480
Stepan Anchugov <kixxx1@gmail.com>
2015-05-13 00:54:42 +0500

    add basic gulp tasks

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,17 @@
 var gulp = require('gulp'),
-    babel = reqire('gulp-babel');
+    babel = require('gulp-babel');
+
+var config = {
+    src: './src',
+    dist: './dist'
+};
+
+gulp.task('scripts', function() {
+    gulp.src(config.src + '/scripts/*.js')
+        .pipe(babel())
+        .pipe(gulp.dest(config.dist + '/js'));
+});
 
 gulp.task('default', function() {
-
+   gulp.watch(config.src + '/scripts/**/*.js', ['scripts']); 
 });
