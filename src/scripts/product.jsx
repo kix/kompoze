5092faee89e7ae41e197cf38a2bb16f0709f15c2
Stepan Anchugov <kixxx1@gmail.com>
2015-05-15 03:27:19 +0500

    handle clicks

--- src/scripts/product.jsx
+++ src/scripts/product.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
 export default class Product extends React.Component {
+    constructor() {
+        super();
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick() {
+        console.log('Clicked', this.props.model);
+    }   
     render() {
         return (
-          <div className="product" data-id={this.props.model.get('id')}>
-              <h2 className="product_title">{this.props.model.get('title')}</h2>
-            </div>
+            <button className="product" data-id={this.props.model.get('id')} onClick={this.handleClick}>
+                {this.props.model.get('title')}
+            </button>
         );
     }
 }
