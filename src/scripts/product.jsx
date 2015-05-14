a874d181b0da94d38760eddc2bbe4d8e8763c873
Stepan Anchugov <kixxx1@gmail.com>
2015-05-15 01:31:08 +0500

    idiomatize backbone getters

--- src/scripts/product.jsx
+++ src/scripts/product.jsx
@@ -4,8 +4,8 @@ import Backbone from 'backbone';
 export default class Product extends React.Component {
     render() {
       return (
-        <div className="product" data-id={this.props.model.id}>
-            <h2 className="product_title">{this.props.model.attributes.title}</h2>
+        <div className="product" data-id={this.props.model.get('id')}>
+            <h2 className="product_title">{this.props.model.get('title')}</h2>
           </div>
       );
     }
